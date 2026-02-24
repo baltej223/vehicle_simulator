18dbbd48d7782927dbdd6fbaf8be4acb74832371
baltej223 <baltej963@gmail.com>
2026-02-25 00:21:34 +0530

    Autonomous Driving algo done, fixing it is left

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Simply simulating roads on which cars can run.
+# Simulating Cars and Roads
+
+- Basically, Cars run on roads, but in C++.
+
+# > [!NOTE]
+
+> There is some logical bug, sometimes the car covers more distance, then the actual length of road.
--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <chrono>
 #include <cstdlib>
 
+#define INFINITY (unsigned)!((int)0)
+#define MINIMUM_DISTANCE_BETWEEN_CARS 100
 /*
 NOTE:
 - I was thinking for multiple cars to run at the sametime, 
@@ -37,9 +39,9 @@ class Road : public PhysicalObject{
     std::vector<float> road_vector;
     int precision_distance;
     int length; // The total length of the road
-    int totalLanes; // between [1, totalLanes]
+    int totalLanes; // between [1, totalLanes], 1: means the leftest lane;
     // Here comes the  array of the vehicals, running on the road.
-    std::vector<Vehicle> onRoadVehicals;
+    std::vector<Vehicle*> onRoadVehicals;
 
     private:
     int minWidth = 10;
@@ -97,13 +99,26 @@ class Vehicle{
   public:
   int road_width;
   int distance_completed=0;
-  int speed; // meters per seconds.
+  int speed = 0; // meters per seconds.
   Road *road;
-  int current_lane = 0;
+  int current_lane = 1;
+  std::string name;
+  static int totalCars;
 
-  Vehicle(Road rd): road(rd) {
-      this->road_width = rd.width;
-      this->road = &rd;
+  Vehicle(Road* rd, std::string name = "default", int distance = 0): road(rd) {
+    totalCars =totalCars + 1;
+  
+    this->road_width = rd->width;
+    this->road = rd;
+    // Now I need to put this object's pointer in the vectors<Vehicle> of the road.
+    this->road->onRoadVehicals.push_back(this);
+    this->distance_completed = distance;
+    
+    if (name == "default") {
+        this->name = "Vehicle " + totalCars;
+    } else {
+      this->name = name;
+    }
   }
   int Run(int lane, int speed) {
     this->speed = speed;
@@ -111,6 +126,8 @@ class Vehicle{
     // if the input from the user is not greater that the number of lanes of the road.
     if (!(lane > 0 && lane <= this->road->totalLanes)){ 
       throw std::invalid_argument("Bad parameters!");
+    } else {
+      this->current_lane = lane;
     }
       // It records the position of the car from left side of the road. 
       // Initially start from some random x from road's left side.
@@ -136,32 +153,101 @@ class Vehicle{
       double time_covering_precision_distance = (double) this->road->precision_distance /  (double)  speed;
       
           observeAndDrive();
-          std::cout << i << "\n";
+          // std::cout << i << "\n";
           this->distance_completed += speed * time_covering_precision_distance;
+          std::cout << "["+this->name+"] " << "Distance Completed:" << distance_completed << std::endl;
+          std::cout << "Speed: " << speed << std::endl;
           std::this_thread::sleep_for(std::chrono::duration<double>(time_covering_precision_distance)); 
     }
       return distance_completed;
   }
-
-  void observeAndDrive() { // Change Lanes
-    // Here I need to check the car's distance from other cars, 
-    // and need to change lanes, if required.
-    
-    for (Vehicle other_car : road->onRoadVehicals ) {
+ 
+  void observeAndDrive() { // Change Lanes and speed
       // Here comes the actual algorithem;
+    std::vector<std::pair<int, int>> distances;
+    for (Vehicle *other_car : road->onRoadVehicals ) {
+      int distance_diff = other_car->distance_completed - this->distance_completed; // -ve means behind, and +ve means ahead 
+      int lane_diff = other_car->current_lane - this->current_lane; // to the left: +ve | to the right: -ve
+      std::pair<int, int> p = {distance_diff, lane_diff};
+      distances.push_back(p);
+    }
 
+    // if there is something ahead (100 meter) going slow, then change lane to left, 
+    // if left is busy, for example, something is comming fast from beghind, or something is going just with you.
+    // then go to right, if right not possible, then slow down.
+    // if possible then slow down;
+    // I guess thats all, I need to implement;
+
+    // so first I need to find whats going ahead of me.
+    int minDistance = INFINITY;
+    for (int i = 0 ; i < distances.size(); i++) {
+      std::pair<int, int> distance = distances[i];
+      if (!(distance.second == this->current_lane)) {
+        continue;
+      }
+      if (distance.second < minDistance) {
+        minDistance = distance.second;
+      }
+    }
+    if (!(minDistance < MINIMUM_DISTANCE_BETWEEN_CARS)) return; // do nothing, 
+                                                                // if all the cars are very far apart
+    if (this->current_lane > 1) {
+      // Now I need to check if there is any car of the left lane 
+      // which is comming fast (has more speed than me), and is near than const MINIMUM_DISTANCE_BETWEEN_CARS,
+      std::vector<Vehicle*> onLeftLane = this->onLane(current_lane-1);
+      for (Vehicle* v:  onLeftLane) {
+        if (v->speed > this->speed && this->mod(v->distance_completed - this->distance_completed) < MINIMUM_DISTANCE_BETWEEN_CARS) {
+          if (this->current_lane < this->road->totalLanes) {
+            std::vector<Vehicle*> onRightLane = this->onLane(current_lane + 1);
+            for (Vehicle* v_r : onRightLane) {
+              if (v_r->speed > this->speed && this->mod(v->distance_completed - this->distance_completed) < MINIMUM_DISTANCE_BETWEEN_CARS) {
+                // slow 
+                this->speed = this->speed - 10;
+              }
+            }
+          } else {
+            // slow 
+            this->speed = this->speed -10;
+
+          }
+        } else {
+          this->current_lane = current_lane - 1; 
+        }
+      }
+    } else if (current_lane == 1){
+      // slow 
+      this->speed = this->speed - 10;
     }
   }
+  private : 
+  std::vector<Vehicle*> onLane(int lane){
+    std::vector<Vehicle*> toReturn;
+    for (Vehicle *v : this->road->onRoadVehicals) {
+      if (v->current_lane == lane) {
+        toReturn.push_back(v);
+      }
+    }
+    return toReturn;
+  }
+  int mod(int num) {
+    return num < 0 ? num * -1 : num;  
+  }
 };
+
+  int Vehicle::totalCars = 0;
 
 int main(){
     Road NewRd(200, 500, 4);
     std::vector<float> RdAngle = {90, 90, 0, 0, -90}; 
     NewRd.simpleRoad(100, RdAngle);
 
-    Vehicle Car(NewRd);
+    Vehicle Car1(&NewRd, "Car 1", 100);
+    Vehicle Car2(&NewRd, "Car 2", 0);
     // std::cout << Car.Run(1, 200);
 
-    std::thread t1(&Vehicle::Run, &Car, 1, 200);
+    std::thread t1(&Vehicle::Run, &Car1, 1, 200);
+    std::thread t2(&Vehicle::Run, &Car2, 1, 400);
+    
     t1.join();
+    t2.join();
 }
